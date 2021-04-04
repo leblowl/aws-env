67c12b47a3b5dcd2bfaf7d176fd8a326b2eec51a
Lucas Leblow <lucasleblow@mailbox.org>
2021-04-03 19:48:21 -0600

    Add NAT EC2 server

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,4 +2,8 @@
 #
 # Usage: bin/deploy.sh cf/vpc.yaml env/vpc.example.json net-main
 
-aws cloudformation deploy --template-file $1 --parameter-overrides file://$2 --stack-name $3
+aws cloudformation deploy \
+    --template-file $1 \
+    --parameter-overrides file://$2 \
+    --stack-name $3 \
+    --capabilities CAPABILITY_NAMED_IAM
